03af032c18c8032d8b5f17d0abca6456f54162e3
Lentil Plantain <lenypan@pacbell.net>
2018-07-13 11:58:46 -0700

    sd

--- 6-javascript_continued/0-parameter.js
+++ 6-javascript_continued/0-parameter.js
@@ -1,3 +1,3 @@
 <!DOCTYPE javascript>
 var my_parameter = process.argv[2];
-console.log("Here is the first passed parameter " + my_parameter)
+console.log("Here is the first passed parameter: " + my_parameter)
